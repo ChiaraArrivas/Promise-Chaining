cb376469be13237bf943b6a7d7de52214476e835
Chiara Arrivas <ritarrivas96@gmail.com>
2022-11-05 11:53:17 +0100

    Promise Chaining

---
+++ exercise.js
@@ -0,0 +1,39 @@
+const isLogged = true;
+
+// FIRST PROMISE
+
+function firstPromise(isLogged) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isLogged == true) {
+                resolve(Math.random())
+            } else {
+                reject(new Error("Error"))
+            }
+        }, 2000)
+    })
+}
+
+//SECOND PROMISE
+
+function secondPromise(number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (number > 0.5) {
+                resolve({ name: "John", age: 24 })
+            } else {
+                reject(new Error("Il numero è minore di 0.5"))
+            }
+        }, 1000)
+    })
+}
+
+firstPromise(isLogged)   //first promise
+    .then((random) => {
+        console.log(random)  //mostrami il numero random
+        return random;       //restituisci alla second promis il numero random
+    })
+    .then((number) => secondPromise(number))  //Stamperà a seconda del numero random
+    .then((user) => console.log(user))       // Quando la second promis da il via allora stamperà l'user di firstPromise, altrimenti reject di secondPromise
+    .catch((err) => console.error(err.message))         //Cattura e stampa errore
+    
